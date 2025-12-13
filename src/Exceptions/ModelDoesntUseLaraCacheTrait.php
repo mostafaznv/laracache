2b7cf66d362e065ffd6f1ad3a9ce8b7b6fe85f17
mostafaznv <mostafa.zeinivand@gmail.com>
2025-12-13 17:09:13 +0330

    refactor: improve exception messages for clarity and consistency

--- src/Exceptions/ModelDoesntUseLaraCacheTrait.php
+++ src/Exceptions/ModelDoesntUseLaraCacheTrait.php
@@ -4,10 +4,11 @@ namespace Mostafaznv\LaraCache\Exceptions;
 
 use InvalidArgumentException;
 
+
 class ModelDoesntUseLaraCacheTrait extends InvalidArgumentException
 {
     public static function make(string $model): static
     {
-        return new static("Target model [$model] does not use LaraCache Trait.");
+        return new static("The specified model [$model] does not use the `LaraCache` trait.");
     }
 }
