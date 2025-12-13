b5091c46472008d7ceaaeb675d4b44f732a930bf
mostafaznv <mostafa.zeinivand@gmail.com>
2025-12-13 23:57:37 +0330

    refactor: adjust command signature spacing for consistency across all cache commands

--- src/Commands/DeleteGroupCacheCommand.php
+++ src/Commands/DeleteGroupCacheCommand.php
@@ -6,10 +6,12 @@ use Illuminate\Console\Command;
 use Mostafaznv\LaraCache\Actions\DeleteGroupCacheAction;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 
+
 class DeleteGroupCacheCommand extends Command
 {
-    protected $signature = 'laracache:delete-group {group}';
+    protected $signature   = 'laracache:delete-group {group}';
     protected $description = 'Deletes all models and entities in a group';
+
 
     public function handle(): int
     {
--- src/Commands/UpdateCacheCommand.php
+++ src/Commands/UpdateCacheCommand.php
@@ -7,10 +7,12 @@ use Mostafaznv\LaraCache\Actions\UpdateCacheAction;
 use Mostafaznv\LaraCache\DTOs\CommandData;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 
+
 class UpdateCacheCommand extends Command
 {
-    protected $signature = 'laracache:update {--m|model=*} {--e|entity=*}';
+    protected $signature   = 'laracache:update {--m|model=*} {--e|entity=*}';
     protected $description = 'Updates one or multiple cache entities in a single model';
+
 
     public function handle(): int
     {
